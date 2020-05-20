1426ba8e104b9b09a7200ccf096dfb857c182ffa
unknown <michelangelo.ricci@libero.it>
2020-05-20 15:20:04 +0200

    modifica servlet

---
+++ src/main/java/it/contrader/servlets/ArticleServlet.java
@@ -0,0 +1,90 @@
+package it.contrader.servlets;
+
+import java.util.List;
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import it.contrader.dto.ArticleDTO;
+import it.contrader.service.Service;
+import it.contrader.service.ArticleService;
+
+public class ArticleServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ArticleServlet() {
+	}
+	
+	public void updateList(HttpServletRequest request) {
+		Service<ArticleDTO> service = new ArticleService();
+		List<ArticleDTO>listDTO = service.getAll();
+		request.setAttribute("list", listDTO);
+	}
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Service<ArticleDTO> service = new ArticleService();
+		String mode = request.getParameter("mode");
+		ArticleDTO dto;
+		int id;
+		boolean ans;
+
+		switch (mode.toUpperCase()) {
+
+		case "ARTICLELIST":
+			updateList(request);
+			getServletContext().getRequestDispatcher("/article/usermanager.jsp").forward(request, response);                  //qui metto article?
+			break;
+
+		case "READ":
+			id = Integer.parseInt(request.getParameter("id"));
+			dto = service.read(id);
+			request.setAttribute("dto", dto);
+			
+			if (request.getParameter("update") == null) {
+				 getServletContext().getRequestDispatcher("/article/readuser.jsp").forward(request, response);
+				
+			}
+			
+			else getServletContext().getRequestDispatcher("/article/updateuser.jsp").forward(request, response);
+			
+			break;
+
+		case "INSERT":
+			String name = request.getParameter("name").toString();
+			String description = request.getParameter("description").toString();
+			int price =Integer.parseInt(request.getParameter("price"));
+			dto = new ArticleDTO (name,description,price);
+			ans = service.insert(dto);
+			request.setAttribute("ans", ans);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/article/usermanager.jsp").forward(request, response);
+			break;
+			
+		case "UPDATE":
+			name = request.getParameter("name");
+			description = request.getParameter("description");
+			price = Integer.parseInt(request.getParameter("price"));
+			id = Integer.parseInt(request.getParameter("id"));
+			dto = new ArticleDTO (id,name, description, price);
+			ans = service.update(dto);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/article/usermanager.jsp").forward(request, response);
+			break;
+
+		case "DELETE":
+			id = Integer.parseInt(request.getParameter("id"));
+			ans = service.delete(id);
+			request.setAttribute("ans", ans);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/article/usermanager.jsp").forward(request, response);
+			break;
+		}
+	}
+
+}
---
+++ src/main/java/it/contrader/servlets/PlatformServlet.java
@@ -0,0 +1,91 @@
+package it.contrader.servlets;
+
+import java.util.List;
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import it.contrader.dto.PlatformDTO;
+import it.contrader.service.Service;
+import it.contrader.service.PlatformService;
+
+public class PlatformServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	public PlatformServlet() {
+	}
+	
+	public void updateList(HttpServletRequest request) {
+		Service<PlatformDTO> service = new PlatformService();
+		List<PlatformDTO>listDTO = service.getAll();
+		request.setAttribute("list", listDTO);
+	}
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Service<PlatformDTO> service = new PlatformService();
+		String mode = request.getParameter("mode");
+		PlatformDTO dto;
+		int id;
+		boolean ans;
+
+		switch (mode.toUpperCase()) {
+
+		case "PLATFORMLIST":
+			updateList(request);
+			getServletContext().getRequestDispatcher("/platform/usermanager.jsp").forward(request, response);
+			break;
+
+		case "READ":
+			id = Integer.parseInt(request.getParameter("id"));
+			dto = service.read(id);
+			request.setAttribute("dto", dto);
+			
+			if (request.getParameter("update") == null) {
+				 getServletContext().getRequestDispatcher("/platform/readuser.jsp").forward(request, response);
+				
+			}
+			
+			else getServletContext().getRequestDispatcher("/platform/updateuser.jsp").forward(request, response);
+			
+			break;
+
+		case "INSERT":
+			String name = request.getParameter("name").toString();
+			String description = request.getParameter("description").toString();
+			String website = request.getParameter("website").toString();
+			dto = new PlatformDTO (name,description,website);
+			ans = service.insert(dto);
+			request.setAttribute("ans", ans);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/platform/usermanager.jsp").forward(request, response);                 // qui bisogna mettere paltform manager?
+		
+			break;
+			
+		case "UPDATE":
+			name = request.getParameter("name");
+			description = request.getParameter("description");
+			website = request.getParameter("website");
+			id = Integer.parseInt(request.getParameter("id"));
+			dto = new PlatformDTO (id,name, description, website);
+			ans = service.update(dto);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/platform/usermanager.jsp").forward(request, response);                   // vedi sopra
+			break;
+
+		case "DELETE":
+			id = Integer.parseInt(request.getParameter("id"));
+			ans = service.delete(id);
+			request.setAttribute("ans", ans);
+			updateList(request);
+			getServletContext().getRequestDispatcher("/platform/usermanager.jsp").forward(request, response);
+			break;
+		}
+	}
+
+}
