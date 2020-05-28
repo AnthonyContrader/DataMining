9bc4f057b082382de5b7b574b09e2c9a4bbf5a29
unknown <michelangelo.ricci@libero.it>
2020-05-28 15:30:58 +0200

    inizio terza settimana

--- src/main/java/it/contrader/dto/UserDTO.java
+++ src/main/java/it/contrader/dto/UserDTO.java
@@ -3,9 +3,11 @@ package it.contrader.dto;
 import it.contrader.model.User.Usertype;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 
